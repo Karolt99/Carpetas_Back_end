b98f1080b5327aa2db4b963c27c08077d1145233
Karol Tatiana Garzon Barrios <131315140+Karolt99@users.noreply.github.com>
2023-06-29 19:53:54 -0500

    nuevo

--- proyectofinal/routes/pacientes.js
+++ proyectofinal/routes/pacientes.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 const { conexion } = require('../database/conexion.js')
 
-//Listando todas las mascotas
+//Listando todos los pacientes
 router.get('/', function(req, res, next) {
   conexion.query('SELECT * FROM pacientes', (error, pacientes) => {
     if(error){
@@ -13,4 +13,39 @@ router.get('/', function(req, res, next) {
   })
 });
 
+//Insertar pacientes
+
+router.get('/agregar', (req, res) => {
+  res.status(200).sendFile('registro-pacientes.html', {root: 'public'})
+})
+
+router.post('/guardar-paciente', (req, res) => {
+  const id = req.body.id
+  const nombre = req.body.nombre
+  const apellido = req.body.apellido
+  const edad = req.body.edad
+  const telefono = req.body.telefono
+  const especialidad = req.body.especialidad
+
+  conexion.query(`INSERT INTO pacientes (id, nombre, apellido, edad, telefono, especialidad) VALUES (${id}, '${nombre}', '${apellido}', ${edad}, '${telefono}', '${especialidad}')`, (error, resultado) => {
+    if (error) {
+      res.status(500).send('Ocurrio un error en la consulta'+ error)
+    } else {
+      res.status(200).redirect('/pacientes')
+    }
+  })
+})
+
+//Eliminando pacientes
+
+router.get('/eliminar/:id', (req, res) => {
+  const id = req.params.id
+  conexion.query(`DELETE FROM pacientes WHERE id=${id}`, (error, resultado) => {
+    if(error){
+      res.status(500).send('Ocurrio un error en la consulta ' + error)
+    } else {
+      res.status(200).redirect('/pacientes')
+    }
+  })
+})
 module.exports = router;
